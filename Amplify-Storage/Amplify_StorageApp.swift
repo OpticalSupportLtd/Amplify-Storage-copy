c207410246e86c29a11273e7bab1bff3c8077ee4
OpticalSupportLtd <Osproducts+georgina@opticalsupport.com>
2024-08-09 15:16:34 +0100

    Initial Commit

---
+++ Amplify-Storage/Amplify_StorageApp.swift
@@ -0,0 +1,17 @@
+//
+//  Amplify_StorageApp.swift
+//  Amplify-Storage
+//
+//  Created by Georgina Contreras-Keenan on 09/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct Amplify_StorageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
